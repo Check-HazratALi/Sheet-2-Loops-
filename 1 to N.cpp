b7379c7ff66ca2419d8efcba9934589b763ee6e7
Check-HazratALi <162535361+Check-HazratALi@users.noreply.github.com>
2024-03-30 19:49:50 +0600

    A

---
+++ 1 to N.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int num,sum=0;
+    cin>>num;
+    for (int i = 0; i < num; i++)
+    {
+        sum=i+1;
+        cout<<sum<<endl;
+    }
+    return 0;
+}
